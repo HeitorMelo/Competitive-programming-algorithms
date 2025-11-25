40c580ea41694df4c46099e618f3a6f384ee172f
HeitorMelo <heitormelo2468@gmail.com>
2025-11-24 22:26:46 -0300

    Add pdf module (SamuellH12) and annotation

---
+++ Library/Annotation/annotation.cpp
@@ -0,0 +1,3 @@
+/*LATEX_IGNORED_BEGIN************************
+g++ "$1.cpp" -o "$1" -Wall -Wextra -O2 -Wshadow -fsanitize=address,undefined
+***************************LATEX_IGNORED_END*/
---
+++ pdf/generate_pdf.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+set -e
+
+g++ -std=c++17 -o genLatex generate_latex.cpp -O2 -lcrypto
+./genLatex
+
+pdflatex -halt-on-error -file-line-error notebook.tex
+latexmk -pdf -f -silent notebook.tex
+mv notebook.pdf ../Notebook.pdf
+rm contents.tex
+mv notebook.tex _notebook.tex
+rm notebook.*
+mv _notebook.tex notebook.tex
+rm genLatex.exe
+mv hsh.temp ./temp/h || true
+mv temp.cpp ./temp/h || true
+rm ./temp -r
+
+echo "
+Spinosaurus says: All done! =]
+Good luck and good contest!
+"

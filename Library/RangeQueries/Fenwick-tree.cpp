d34e1f4425de0562b5ad9f65ad5e9f2d919c8ad7
HeitorMelo <heitormelo2468@gmail.com>
2026-04-20 19:43:19 -0300

    Update fenwick

--- Library/RangeQueries/Fenwick-tree.cpp
+++ Library/RangeQueries/Fenwick-tree.cpp
@@ -5,19 +5,20 @@ using namespace std;
 
 struct Fenwick {
     int n;
+    int max_pow; // for searching only
     vector<ll> bit;
 
     Fenwick() {}
-    Fenwick(int n): n(n), bit(n + 1, 0) {}
+    Fenwick(int _n): n(_n), bit(_n + 1, 0) {
+        max_pow = 1 << (31 - __builtin_clz(n));
+    }
 
-    // add value to index idx (1-indexed)
     void add(int idx, ll val) {
         for (; idx <= n; idx += idx & -idx) {
             bit[idx] += val;
         }
     }
 
-    // sum of [1..idx]
     ll pref_sum(int idx) const {
         ll ans = 0;
         for (; idx > 0; idx -= idx & -idx) {
@@ -29,5 +30,25 @@ struct Fenwick {
     ll range_sum(int l, int r) const {
         if (l > r) return 0;
         return pref_sum(r) - pref_sum(l - 1);
+    }
+
+    int end() const { return n + 1; }
+
+    int lower_bound(ll k) {
+        if (k > pref_sum(n)) return end();
+        if (k <= 0) return 0;
+        int pos = 0;
+        
+        for (int i = max_pow; i > 0; i >>= 1) {
+            if (pos + i <= n && bit[pos + i] < k) {
+                pos += i;
+                k -= bit[pos];
+            }
+        }
+        return pos + 1;
+    }
+
+    int upper_bound(ll k) {
+        return lower_bound(k + 1LL);
     }
 };
